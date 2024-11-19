811b22019a851ac1424eda5d54677f890d05f79c
Román Zamora <roman.zamora@roman.zamora-HT4RW0K91R>
2024-11-19 15:54:36 +0100

    .

--- main.tf
+++ main.tf
@@ -1,4 +1,14 @@
 // Test v1
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0.0" 
+    }
+
+  }
+}
+
 provider "azurerm" {
     features = {}
 }
